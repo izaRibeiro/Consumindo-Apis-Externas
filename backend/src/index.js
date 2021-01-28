c9aed57e4d43362b53949c6bfdf101fb02b4694b
izaRibeiro <bellarpjf@gmail.com>
2021-01-27 21:09:22 -0300

    Configurando conexão com o mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.get("/", (request, response) =>
     response.json({ message: "Aplicação rodando corretamente" })
+);
+
+mongoose.connect(
+    "mongodb+srv://iza:iza@cluster0-xcpnh.mongodb.net/apiExterna?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
 );
 
 app.listen(3333);
